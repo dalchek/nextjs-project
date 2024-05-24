edc8b6ca1b863d065aced1a7f83ce62e0f0553e8
Dalchek <dalchek999@gmail.com>
2024-05-24 18:11:26 +0200

    nextjs full stack

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} p-5 bg-gray-800 text-white`}>{children}</body>
     </html>
   );
 }
