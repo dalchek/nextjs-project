e69f24ef0d099630b40875e6bca88e16c49a757b
Dalchek <dalchek999@gmail.com>
2024-05-28 11:44:13 +0200

    js app

---
+++ src/app/components/container.tsx
@@ -0,0 +1,9 @@
+export default function Container({children}: {
+    children: React.ReactNode
+}) {
+  return (
+    <div className="max-w-[1100px] mx-auto bg-gray-800 min-h-screen flex flex-col border-l border-r">
+        {children}
+    </div>
+  )
+}
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import PostsList from "../components/posts-list"
+import React, { Suspense } from 'react'
 
-export default function Page() {
+export default async function Page() {
+  
+
   return (
-    <main className="text-center pt-32 px-5">
+    <main className="text-center pt-16 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">All Posts</h1>
+
+      <Suspense fallback="Loading...">
+        <PostsList />
+      </Suspense>
     </main>
+
   )
 }
